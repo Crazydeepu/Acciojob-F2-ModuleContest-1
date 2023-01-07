64751dc665076ea49298fa08998397dc452442f5
Anudeep_Reddy <anudeepreddypocha@gmail.com>
2023-01-07 20:21:43 +0530

    First Commit

--- script.js
+++ script.js
@@ -8,20 +8,52 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  arr.map(printingThroughMap);
+
+  function printingThroughMap(arrayItem) {
+    if(arrayItem.profession === "developer") {
+      console.log(arrayItem);
+    }
+  }
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach(printingThroughForEach);
+
+  function printingThroughForEach(arrayItem) {
+    if(arrayItem.profession === "developer") {
+      console.log(arrayItem);
+    }
+  }
 }
 
 function addData() {
   //Write your code here
+  let newObj = { id: 4, name: "susan", age:"20",profession:"intern"};
+  arr.push(newObj);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here
+  let filtered_arr = arr.filter(function (val) {
+
+    if(val.profession !== "admin") {
+      return val;
+    }
+  });
+  console.log(filtered_arr);
 }
 
 function concatenateArray() {
   //Write your code here
+  let arr2 = [
+    { id: 5, name: "Sudharshan", age:"30", profession:"Programmer"},
+    { id: 6, name: "KrishnaSree", age:"28", profession:"developer"},
+    { id: 7, name: "Sandhya", age:"32", profession:"Tester"},
+  ];
+
+  let concatArray = arr.concat(arr2);
+  console.log(concatArray);
 }
